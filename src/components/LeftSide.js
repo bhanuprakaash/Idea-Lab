373802a6f3f9fc8f2969730cd99c0c036816412a
Bhanu Prakash Sai <bhanuprakashsai7@gmail.com>
2023-03-12 19:44:55 +0530

    added a leftside

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -12,213 +12,102 @@ const Leftside = (props) => {
   return (
     <Container>
       <ArtCard>
-        <UserInfo>
-          <CardBackground />
-          <a>
-            <Photo>
-              {
-                props.user && props.user.photoURL ? (
-                  <img src={props.userDetails.photoUrl} alt="" 
-                    style={
-                      {
-                        width: "100%",
-                        height: "100%",
-                        borderRadius: "50%",
-                        objectFit: "cover",
-                      }
-                    }/>
-                ) : (
-                  <img src="/images/user.svg" alt="" />
-                )
-              }
-            </Photo>
-            <Link>Welcome,{props.user ? props.userDetails.name:"there"}!</Link>
-          </a>
-          <a>
-            <AddPhotoText>Add a photo</AddPhotoText>
-          </a>
-        </UserInfo>
-        <Widget>
-          <a>
-            <div>
-              <span>Connections</span>
-              <span>Grow your network</span>
-            </div>
-            <img src="/images/widget-icon.svg" alt="" />
-          </a>
-        </Widget>
-        <Item>
-          <span>
-            <img src="/images/item-icon.svg" alt="" />
-            My Items
-          </span>
-        </Item>
+        <div>
+          <img src="/images/icons8-add(1).svg" alt="add" width="100px" />
+        </div>
+        <Info>
+          <h2>Hey,<br></br>Found anything new?</h2>
+          <h3>Let's share it with your tech community! </h3>
+          <p>
+          <img src="/images/camera.svg" alt="" />
+          <span>Photo</span>
+          </p>
+          <p><img src="/images/video-icon.svg" alt="" />
+          <span>Video</span></p>
+          <p>
+          <img src="/images/icons8-idea(1).svg" alt=""/>
+          <span>Idea</span>
+          </p>
+        </Info>
       </ArtCard>
-
-      <CommunityCard>
-        <a>
-          <span>Groups</span>
-        </a>
-        <a>
-          <span>
-            Events
-            <img src="/images/plus-icon.svg" alt="" />
-          </span>
-        </a>
-        <a>
-          <span>Follow Hashtags</span>
-        </a>
-        <a>
-          <span>Discover more</span>
-        </a>
-      </CommunityCard>
+      
     </Container>
   );
 };
 
 const Container = styled.div`
   grid-area: leftside;
+  margin-top: 10px;
+  width: 100%;
 `;
 
 const ArtCard = styled.div`
+  height: 400px;
   text-align: center;
   overflow: hidden;
   margin-bottom: 8px;
-  background-color: #fff;
+  background-image: white;
+  border:2px solid rgb(102,103,171);
   border-radius: 5px;
   transition: box-shadow 83ms;
-  position: relative;
-  border: none;
+  position: sticky;
+  top:110px;
   box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
-`;
-
-const UserInfo = styled.div`
-  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
-  padding: 12px 12px 16px;
-  word-wrap: break-word;
-  word-break: break-word;
-`;
-
-const CardBackground = styled.div`
-  background: url("/images/card-bg.svg");
-  background-position: center;
-  background-size: 462px;
-  height: 54px;
-  margin: -12px -12px 0;
-`;
-
-const Photo = styled.div`
-  box-shadow: none;
-  background-image:"";
-  width: 72px;
-  height: 72px;
-  box-sizing: border-box;
-  background-clip: content-box;
-  background-color: white;
-  background-position: center;
-  background-size: 60%;
-  background-repeat: no-repeat;
-  border: 2px solid white;
-  margin: -38px auto 12px;
-  border-radius: 50%;
-`;
-
-const Link = styled.div`
-  font-size: 16px;
-  line-height: 1.5;
-  color: rgba(0, 0, 0, 0.9);
-  font-weight: 600;
-`;
-
-const AddPhotoText = styled.div`
-  color: #0a66c2;
-  margin-top: 4px;
-  font-size: 12px;
-  line-height: 1.33;
-  font-weight: 400;
-`;
-
-const Widget = styled.div`
-  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
-  padding-top: 12px;
-  padding-bottom: 12px;
-  & > a {
-    text-decoration: none;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 4px 12px;
-    &:hover {
-      background-color: rgba(0, 0, 0, 0.08);
+  div{
+    height: 15%;
+    img{
+      object-fit: contain;
+      height: 100%;
+      cursor: pointer;
     }
-    div {
-      display: flex;
-      flex-direction: column;
-      text-align: left;
-      span {
-        font-size: 12px;
-        line-height: 1.333;
-        &:first-child {
-          color: rgba(0, 0, 0, 0.6);
-        }
-        &:nth-child(2) {
-          color: rgba(0, 0, 0, 1);
-        }
-      }
-    }
-  }
-  svg {
-    color: rgba(0, 0, 0, 1);
+    text-align: center;
+    border:2px solid black;
+    border-radius: 5px;
+    padding: 15px;
+    margin: 20px;
   }
 `;
 
-const Item = styled.a`
-  border-color: rgba(0, 0, 0, 0.8);
-  text-align: left;
-  padding: 12px;
-  font-size: 12px;
-  display: block;
-  span {
-    display: flex;
-    align-items: center;
-    color: rgba(0, 0, 0, 1);
-    svg {
-      color: rgba(0, 0, 0, 0.6);
-    }
-  }
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.08);
-  }
-`;
-
-const CommunityCard = styled(ArtCard)`
-  padding: 8px 0 0;
-  text-align: left;
-  display: flex;
-  flex-direction: column;
-  a {
+const Info = styled.section`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  padding: 0 16px;
+  border:none;
+  h2{
+    font-family:Raleway;
+    font-size: 24px;
+    font-weight: 500;
     color: black;
-    padding: 4px 12px 4px 12px;
-    font-size: 12px;
-    &:hover {
-      color: #0a66c2;
+    line-height: 1.5;
+    margin-bottom: 5px;
+  }
+  h3{
+    font-size: 15px;
+    font-weight: 350;
+    color: black;
+    line-height: 1.5;
+    margin-bottom: 10px;
+  }
+  p {
+        outline: none;
+        color: rgba(0, 0, 0, 0.6);
+        font-size: 14px;
+        line-height: 1.5;
+        background: transparent;
+        border: none;
+        display: flex;
+        align-items: center;
+        font-weight: 600;
+        margin-bottom: 5px;
     }
     span {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
+        margin-left: 5px;
+        font-size: 18px;
     }
-    &:last-child {
-      color: rgba(0, 0, 0, 0.6);
-      text-decoration: none;
-      border-top: 1px solid #d6cec2;
-      padding: 12px;
-      &:hover {
-        background-color: rgba(0, 0, 0, 0.08);
-      }
-    }
-  }
+  text-align: left;
+  padding: 20px;
+  padding-top: 10px;
 `;
+
 const mapStateToProps = (state) => {
   return {
     user: state.userState.user,
